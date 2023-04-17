311c1b5cb8753bfab2214e3627c56d39d0882b0c
sunil <sunil@dzangolab.com>
2023-04-17 23:54:08 +0545

    fix: footer position

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar"
 import Home from './components/Home';
 import Bookings from './components/Bookings';
